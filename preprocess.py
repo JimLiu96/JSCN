ef1e91136bf58dd2b0a7c8b8c8baa284f319b3a6
Zhiwei Liu <jim96liu@gmail.com>
2018-12-01 21:01:17 -0600

    update the preprocessing package so that it can find the target common user from source

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,7 @@
 import json
 import gzip
 import pickle
+import numpy as np
 
 def split(item_list):
     train = item_list[:int(len(item_list) * 0.8)]
@@ -108,6 +109,88 @@ def loadID2UandI(metaName):
         id2item = pickle.load(id2itemFile)
     return id2user, id2item
 
+def targetRatingUser(sourceUserSet, ratingFileTarget, rating_score=5):
+    # this funtion returns the user to items dict by all the given source users
+    u2items = {}
+    # item2id = {}
+    # id2user = {}
+    # id2item = {}
+    # users, items = set(), set()
+    ratings = 0
+    f = open(ratingFileTarget,'r')
+    for l in f.readlines():
+        u,i,r = l.split(',')[0:3]
+        r = float(r)
+        if u in sourceUserSet:
+            # users.add(u)
+            # items.add(i)
+            ratings += 1
+            if r >= rating_score:
+                if u in u2items:
+                    u2items[u].append(i)
+                else:
+                    u2items[u] = [i]
+    return u2items
+
+def source2targetUserRating(sourceMetaName, targetRatingFile, percent=1.0, train_items=5,user_thres = 5, rating_score=5):
+    sourceID2USER, _ = loadID2UandI(sourceMetaName)
+    sourceUserList = [u for num,u in sourceID2USER.items()]
+    num_user = len(sourceUserList)
+    setIdx = np.random.permutation(num_user)
+    # print(type(setIdx))
+    sourceUserSet = set(np.array(sourceUserList)[setIdx])
+    targetU2Items = targetRatingUser(sourceUserSet, targetRatingFile,rating_score)
+    iid = 0
+    item2id = {}
+    id2user = {}
+    id2item = {}
+    targetI2users = filterItems(targetU2Items, user_thres)
+    for k,items in targetU2Items.items():
+        temp = []
+        for i in items:
+            if i in targetI2users:
+                if i not in item2id:
+                    item2id[i] = iid
+                    iid += 1
+                    temp.append(item2id[i])
+        targetU2Items[k] = temp
+    id2item = {idNum:itemNum for itemNum,idNum in item2id.items()}
+    num_ratings = 0
+    for u in targetU2Items:
+        num_ratings += len(targetU2Items[u])
+    sourceMetaName = pathMetaN2MetaN(sourceMetaName, False)
+    s2tMetaName = targetRatingFile.split('.')[0] + '_from_' + sourceMetaName
+    trainFileName = s2tMetaName + '_train_user.dat'
+    testFileName =  s2tMetaName + '_test_user.dat'
+    userDumpFileName = s2tMetaName +'_id2user.pickle'
+    itemDumpFileName = s2tMetaName + '_id2item.pickle'
+    uid = 0
+    # print('total target Statistic:',len(targetU2Items),iid,ratings)
+    with open(trainFileName, 'w') as f_train:
+        with open(testFileName, 'w') as f_test:
+            for k, v in targetU2Items.items():
+                if k not in id2user:
+                    id2user[uid] = k
+                if len(v) <= train_items:
+                    # print("less than %d items put into train only" %( train_items))
+                    f_train.write(str(uid) + ' ' + ' '.join([str(i) for i in v]) + '\n')
+                else:
+                    train, test = split(v)
+                    f_train.write(str(uid) + ' ' + ' '.join([str(i) for i in train]) + '\n')
+                    f_test.write(str(uid) + ' ' + ' '.join([str(i) for i in test]) + '\n')
+                uid += 1
+    print('starting dump the id of users and items')
+    with open(userDumpFileName,'wb') as userDumpFile:
+        pickle.dump(id2user,userDumpFile)
+    with open(itemDumpFileName,'wb') as itemDumpFile:
+        pickle.dump(id2item,itemDumpFile)
+    print('Target Statistic:',uid,iid)
+    print(num_ratings/(uid*iid))
+    print(uid/num_user)
+    return targetU2Items
+
+
+
 def commonUserItem(metaName1, metaName2):
     id2userMetaName1, id2itemMetaName1 = loadID2UandI(metaName1)
     id2userMetaName2, id2itemMetaName2 = loadID2UandI(metaName2)
@@ -135,3 +218,9 @@ def commonUserIDpair(metaName1, metaName2):
     with open(commonUserIDpairDumpFileName, 'wb') as dumpF:
         pickle.dump(commonUserIdxPair, dumpF)
     return commonUserIdxPair
+
+def pathMetaN2MetaN(pathMetaName, isFile = False):
+	if isFile == False:
+		return  pathMetaName.split("/")[-1]
+	else:
+		return pathMetaN2MetaN(pathMetaName.split('.')[0], isFile=False)
